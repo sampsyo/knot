9192a82da93cecad610475822dd13d99177e117d
Adrian Sampson <adrian@radbox.org>
2015-07-08 16:42:18 -0700

    Parse some options

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate pulldown_cmark;
-
-use pulldown_cmark::Parser;
-use pulldown_cmark::html;
+extern crate getopts;
 
 use std::io;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::fs::File;
+use std::env;
 
-fn render_html(text: &str) -> String {
+use pulldown_cmark::Parser;
+use pulldown_cmark::html;
+use getopts::Options;
+
+// Stolen from the pulldown_cmark example.
+fn markdown_to_html(text: &str) -> String {
     let mut s = String::with_capacity(text.len() * 3 / 2);
     let p = Parser::new(&text);
     html::push_html(&mut s, p);
@@ -23,7 +27,24 @@ fn read_file(filename: &str) -> Result<String, io::Error> {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("o", "out", "output directory", "PATH");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => {
+            writeln!(&mut std::io::stderr(), "{}", f).unwrap();
+            return;
+        }
+    };
+
+    let outdir = matches.opt_str("out").unwrap_or("_public".to_string());
+    let outpath = Path::new(&outdir);
+    println!("{:?}", outpath);
+
     let md = read_file("test.md");
-    let html = render_html(&md.unwrap());
+    let html = markdown_to_html(&md.unwrap());
     println!("{}", html);
 }
